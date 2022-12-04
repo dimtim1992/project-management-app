35028291e5bf9f7c49f20712fadd2c74c4c74429
dimtim1992 <dimtim7272807@gmail.com>
2022-12-04 17:28:51 +0300

    feat: add validation to sign up from

--- src/pages/signUpPage/signUpPage.tsx
+++ src/pages/signUpPage/signUpPage.tsx
@@ -4,8 +4,9 @@ import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router';
 import { signUp } from 'services/api';
 import { langSelector } from 'store/selectors';
-import { useAppDispatch } from 'types/types';
+import { ISignUp, useAppDispatch } from 'types/types';
 import './signUpPage.css';
+import { useForm, SubmitHandler } from 'react-hook-form';
 
 export function SignUpPage() {
   const navigate = useNavigate();
@@ -13,30 +14,63 @@ export function SignUpPage() {
   const langKey = useSelector(langSelector);
   const lang = selectLang(langKey);
 
-  const sign = () => {
+  const sign: SubmitHandler<ISignUp> = () => {
     dispatch(signUp({ name, login, password }));
     navigate('/signin');
   };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ISignUp>();
 
   const [name, setName] = useState('');
   const [login, setLogin] = useState('');
   const [password, setPassword] = useState('');
 
   return (
-    <div className="sign-up-container">
+    <form onSubmit={handleSubmit(sign)} className="sign-up-container">
       <h2>{lang.signUp.name}</h2>
       <p>{lang.signUp.title}</p>
 
-      <label>{lang.signUp.userName}</label>
-      <input type="text" onChange={(e) => setName(e.target.value)} required />
+      <label>
+        {lang.signUp.userName}
+        <input
+          type="text"
+          {...register('name', { required: true, minLength: 3 })}
+          onChange={(e) => setName(e.target.value)}
+          name="name"
+        />
+        {errors.name?.type === 'required' && <span>{lang.signUp.validationRequired}</span>}
+        {errors.name?.type === 'minLength' && <span>{lang.signUp.validationMinLength}</span>}
+      </label>
 
-      <label>{lang.signUp.login}</label>
-      <input type="text" onChange={(e) => setLogin(e.target.value)} required />
+      <label>
+        {lang.signUp.login}
+        <input
+          type="text"
+          {...register('login', { required: true, minLength: 3 })}
+          onChange={(e) => setLogin(e.target.value)}
+          name="login"
+        />
+        {errors.login?.type === 'required' && <span>{lang.signUp.validationRequired}</span>}
+        {errors.login?.type === 'minLength' && <span>{lang.signUp.validationMinLength}</span>}
+      </label>
 
-      <label>{lang.signUp.password}</label>
-      <input type="text" onChange={(e) => setPassword(e.target.value)} required />
+      <label>
+        {lang.signUp.password}
+        <input
+          type="password"
+          {...register('password', { required: true, minLength: 3 })}
+          onChange={(e) => setPassword(e.target.value)}
+          name="password"
+        />
+        {errors.login?.type === 'required' && <span>{lang.signUp.validationRequired}</span>}
+        {errors.login?.type === 'minLength' && <span>{lang.signUp.validationMinLength}</span>}
+      </label>
 
-      <button onClick={sign}>{lang.signUp.button}</button>
-    </div>
+      <button>{lang.signUp.button}</button>
+    </form>
   );
 }
